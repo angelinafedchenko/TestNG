8a892cb1e47b096ba510d33d25db58d8c19da618
Angelina <angelinafedcenko@gmail.com>
2021-03-13 15:26:01 +0200

    Homework_10 TestWithParameters_NEW

---
+++ src/test/java/com/softserve/ita/TestWithParameters.java
@@ -0,0 +1,29 @@
+package com.softserve.ita;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class TestWithParameters {
+
+    @Parameters({"type"})
+    @Test
+    public void userType (String type) {
+        System.out.println("User: " + type);
+    }
+
+    @Test(dataProvider="getData")
+    public void instanceDataProvider (int age, String name) {
+        System.out.println("Personal data: Age, Name(" + age + ", " + name + ")");
+    }
+
+    @DataProvider
+    public Object[][] getData() {
+        return new Object[][]{
+                {30, "Ivan"},
+                {40, "Petro"}
+        };
+
+
+    }
+}
